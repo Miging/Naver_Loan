b4c4481405eed4cd30e7c8541ab0645b07ed0b2e
SongMinhyuk <dnfldpden32@gmail.com>
2024-02-21 22:15:20 +0900

    feat:페이지 생성

--- mysite/views.py
+++ mysite/views.py
@@ -6,4 +6,4 @@ from .models import MainContent
 def index(request):
     content_list=MainContent.objects.order_by('-pub_date')
     context={'content_list':content_list}
-    return render(request,'mysite/content_list.html',context)
+    return render(request,'mysite/mainpage.html',context)
